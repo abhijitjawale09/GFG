821eecda54c76fd474078f3e34b8e3c078077c6e
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-06-09 12:18:06 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Elements in the Range/elements-in-the-range.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		// Your code goes here
+		std::set<int> ss;
+		for(int i = 0 ; i < n ;i++){
+		    if(arr[i] >= A && arr[i] <= B){
+		        ss.insert(arr[i]);
+		    }
+		}
+		int diff =(B + 1) - A;
+		return (diff == ss.size());
+		   
+	}
+
+
+		 
+
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
