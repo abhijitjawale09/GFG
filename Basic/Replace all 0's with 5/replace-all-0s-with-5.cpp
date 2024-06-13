02efa0750e5fd5ceb3087609b44ef2af1f51c14d
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-06-13 15:58:38 +0530

    Updated Solution - GfG to GitHub

--- Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
+++ Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -1,30 +1,37 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int convertFive(int n);
-
+ 
 // Driver program to test above function
-int main() {
+
+// } Driver Code Ends
+class Solution{
+  public:
+    /*you are required to complete this method*/
+    int convertFive(int n)
+    {
+    //Your code here
+    string s = to_string(n);
+    for(int i = 0 ; i < s.size(); i++) {
+        if(s[i] == '0'){
+            s[i] = '5';
+        }
+    }
+    return stoi(s);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
     int T;
-    cin >> T;
-    while (T--) {
-        int n;
-        cin >> n;
-        cout << convertFive(n) << endl;
+    cin>>T;
+    while(T--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution obj;
+    	cout<<obj.convertFive(n)<<endl;
     }
 }
 // } Driver Code Ends
-
-
-/*you are required to complete this method*/
-int convertFive(int n) {
-    // Your code here
-    string temp = to_string(n);
-    for(int i = 0 ;i< temp.length();i++){
-        if(temp[i] == '0'){
-            temp[i] = '5';
-        }
-    }
-        return stoi(temp);
-    }
