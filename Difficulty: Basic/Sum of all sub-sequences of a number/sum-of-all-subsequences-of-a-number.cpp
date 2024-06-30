fc37c9808e9de269e3eb2be73ff097132cc686b8
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-06-30 14:06:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of all sub-sequences of a number/sum-of-all-subsequences-of-a-number.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+
+	int subsequenceSum(string s) 
+	{
+	    // Your code goes here
+	    int n = s.size();
+	    int sum = 0 ;
+	    for(int i = 0; i < n; i++) {
+	        sum += s[i] - '0';
+	    }
+	    return sum<<  n - 1;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		Solution ob;
+
+   		cout << ob.subsequenceSum(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
