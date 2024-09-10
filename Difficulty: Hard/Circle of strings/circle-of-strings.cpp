f26298d15ae420ed1b996ad505c0e5f4aa80ee41
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-09-10 13:34:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Circle of strings/circle-of-strings.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int isCircle(vector<string> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int> adj[26];
+        vector<int> indegree(26 , 0) , outdegree(26 , 0);
+        vector<bool>  mark(26 , false);
+        
+        
+        for(int i = 0; i < n ; i++) {
+            int u = arr[i][0] - 'a';
+            int v = arr[i].back() - 'a';
+            
+            adj[u].push_back(v);
+            
+            indegree[u]++;
+            outdegree[v]++;
+            
+            mark[u] = mark[v] = true;
+        }
+        
+        if( indegree != outdegree) {
+            return false;
+        }
+        
+        queue<int> q;
+        vector<bool> vis(n , false);
+        q.push(arr[0][0] - 'a') ;
+        
+        
+        
+        while(!q.empty()) {
+            
+            int node = q.front();
+            vis[node] = true;
+            q.pop();
+            
+            for(int adjnode : adj[node]) {
+                if(vis[adjnode] == true) {
+                    continue;
+                }
+                q.push(adjnode);
+                
+            }
+        }
+        
+        for(int i = 0; i < 26; i++) {
+            if(mark[i] == true && vis[i] == false) {
+                return false;
+            }
+         }
+         return true;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> A;
+        string s;
+
+        for (int i = 0; i < N; i++) {
+            cin >> s;
+            A.push_back(s);
+        }
+
+        Solution ob;
+        cout << ob.isCircle(A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
