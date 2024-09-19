11fa48785d38c23010fe0aec300d955cded07b0f
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-09-19 11:19:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.cpp
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -2,19 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to reverse words in a given string.
-    string reverseWords(string S) 
-    { 
-       stack<string> words;
-    string word = "";
-    
-    // Traverse the string and push each word to the stack
-    for (char ch : S) {
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        stack<string> words;
+        string word = "";
+        for (char ch : str) {
         if (ch == '.') {
             words.push(word);
             word = "";
@@ -22,34 +20,29 @@ class Solution
             word += ch;
         }
     }
-    // Push the last word
     words.push(word);
     
-    // Pop words from the stack and form the result
     string result = "";
-    while (!words.empty()) {
+    while(!words.empty()) {
         result += words.top();
         words.pop();
-        if (!words.empty()) {
+         if (!words.empty()) {
             result += ".";
         }
     }
-    
     return result;
-    } 
+    }
 };
 
 //{ Driver Code Starts.
-int main() 
-{
+int main() {
     int t;
     cin >> t;
-    while (t--) 
-    {
+    while (t--) {
         string s;
         cin >> s;
         Solution obj;
-        cout<<obj.reverseWords(s)<<endl;
+        cout << obj.reverseWords(s) << endl;
     }
 }
 // } Driver Code Ends
