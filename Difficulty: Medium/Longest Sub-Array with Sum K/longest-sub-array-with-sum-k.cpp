4ca6d35c35718932fb7c4570d820c78d4231ea73
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-11-24 13:23:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
@@ -4,51 +4,62 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-    int lenOfLongSubarr(int A[],  int N, int K) 
-    { 
-        // Complete the function
-        unordered_map<int , int> mp;
-        
-        mp[0] = -1;
-        int sum = 0 , ans  = 0 ;
-        
-        for(int i = 0; i < N; i++) {
-            sum += A[i];
-            
-            if(mp.find(sum) == mp.end() ) {
-                mp[sum] = i;
-            }
-            
-            if(mp.find(sum - K) != mp.end()) {
-                ans = max(ans, i- mp[sum - K]);
-            }
-        }
-        return ans;
-    } 
+class Solution {
+  public:
+    int lenOfLongestSubarr(vector<int>& a, int k) {
+        // code here
+      unordered_map<long long, int> prefixSumMap;
+    long long sum = 0;
+    int maxi = 0;
 
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i];
+
+        if (sum == k) {
+            maxi = max(maxi, i + 1);
+        }
+
+        if (prefixSumMap.find(sum - k) != prefixSumMap.end()) {
+            maxi = max(maxi, i - prefixSumMap[sum - k]);
+        }
+
+        if (prefixSumMap.find(sum) == prefixSumMap.end()) {
+            prefixSumMap[sum] = i;
+        }
+    }
+
+    return maxi;
+    }
 };
 
 //{ Driver Code Starts.
 
 int main() {
-	//code
-	
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n, k;
-	    cin>> n >> k;
-	    int a[n];
-	    
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	   Solution ob;
-	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
-	    
-	}
-	
-	return 0;
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
+        Solution solution;
+        cout << solution.lenOfLongestSubarr(arr, k) << "\n";
+        cout << "~\n";
+    }
+
+    return 0;
 }
+
 // } Driver Code Ends
