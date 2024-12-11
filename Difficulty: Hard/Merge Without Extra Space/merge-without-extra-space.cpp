0e39c5d64d335001d17b39d8e85a23460c7863bc
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-12-11 08:17:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -1,67 +1,76 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-        //Function to merge the arrays.
-        void merge(long long arr1[], long long arr2[], int n, int m) 
-        { 
-            // code here 
-            int i =  n - 1;
-            int j = 0;
-            while( i >= 0  && j < m) {
-                if(arr1[i] >= arr2[j]) {
-                    swap(arr1[i] ,arr2[j]);
-                    i--;
-                    j++;
-                }else {
-                    break;
-                }
-               
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size(); int m = b.size();
+        
+        int i = n -1 , j = 0;
+        
+        while(i >= 0 && j < m) {
+            if(a[i] >= b[j]) {
+                swap(a[i] , b[j]);
+                i--;
+                j++;
+            }else{
+                break;
             }
-             sort(arr1 , arr1  + n);
-                sort(arr2 , arr2 + m);
-         } 
+        }
+        
+        sort(a.begin(), a.end());
+        sort(b.begin() , b.end());
+    }
 };
 
 //{ Driver Code Starts.
 
-int main() 
-{ 
-	
-	int T;
-	cin >> T;
-	
-	while(T--){
-	    int n, m;
-	    cin >> n >> m;
-	    
-	    long long arr1[n], arr2[m];
-	    
-	    for(int i = 0;i<n;i++){
-	        cin >> arr1[i];
-	    }
-	    
-	    for(int i = 0;i<m;i++){
-	        cin >> arr2[i];
-	    }
-	    Solution ob;
-	    ob.merge(arr1, arr2, n, m); 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-        for (int i = 0; i < n; i++) 
-            cout<<arr1[i]<<" "; 
-        
-       
-	    for (int i = 0; i < m; i++) 
-		    cout<<arr2[i]<<" "; 
-	    
-	    cout<<endl;
-	}
+    int t;
+    cin >> t; // Inputting the test cases
 
-	return 0; 
-} 
+    while (t--) {
+        vector<int> a, b;
+
+        // Reading the first array as a space-separated line
+        string arr1;
+        getline(cin >> ws, arr1); // Use ws to ignore any leading whitespace
+        stringstream ss1(arr1);
+        int num;
+        while (ss1 >> num) {
+            a.push_back(num);
+        }
+
+        // Reading the second array as a space-separated line
+        string arr2;
+        getline(cin, arr2);
+        stringstream ss2(arr2);
+        while (ss2 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        ob.mergeArrays(a, b);
+
+        // Output the merged result
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
 
 // } Driver Code Ends
