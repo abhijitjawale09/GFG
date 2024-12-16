d7ab7383981818489f8bd91291ca3d483edb4ec5
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-12-16 12:45:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -6,18 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-      int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
-      vector<int>combined;
-      for(int i=0;i<arr1.size();i++)
-      {
-          combined.push_back(arr1[i]);
-      }
-      for(int i=0;i<arr2.size();i++)
-      {
-          combined.push_back(arr2[i]);
-      }
-      sort(combined.begin(),combined.end());
-      return combined[k - 1];
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        // code here
+       int  n = a.size() , m = b.size();
+       int i = 0 , j = 0;
+       int last = 0;
+       while(k--){
+           
+           if(i < n) {
+               
+               if(j < m && a[i] > b[j]) {
+                   last = b[j];
+                   j++;
+               }else {
+                   last = a[i];
+                   i++;
+               }
+           }else if(j < m){
+               last = b[j];
+               j++;
+           }
+       }
+       return last;
     }
 };
 
@@ -34,22 +44,22 @@ int main() {
         cin.ignore();
         string input;
         int num;
-        vector<int> arr1, arr2;
+        vector<int> a, b;
 
         getline(cin, input);
         stringstream s2(input);
         while (s2 >> num) {
-            arr1.push_back(num);
+            a.push_back(num);
         }
 
         getline(cin, input);
         stringstream s3(input);
         while (s3 >> num) {
-            arr2.push_back(num);
+            b.push_back(num);
         }
 
         Solution ob;
-        cout << ob.kthElement(k, arr1, arr2) << endl;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
     }
     return 0;
 }
