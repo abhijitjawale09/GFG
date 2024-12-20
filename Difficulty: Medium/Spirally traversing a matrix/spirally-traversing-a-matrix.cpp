e9c7285374384e423d1339a6456beb1c658708e7
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-12-20 11:52:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -6,46 +6,41 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
         // code here
-        std::vector<int> result;
-    if (matrix.empty()) return result;
-
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
-
-    while (top <= bottom && left <= right) {
-        // Traverse from left to right on the top row
-        for (int i = left; i <= right; ++i) {
-            result.push_back(matrix[top][i]);
-        }
-        top++;
-
-        // Traverse from top to bottom on the right column
-        for (int i = top; i <= bottom; ++i) {
-            result.push_back(matrix[i][right]);
-        }
-        right--;
-
-        if (top <= bottom) {
-            // Traverse from right to left on the bottom row
-            for (int i = right; i >= left; --i) {
-                result.push_back(matrix[bottom][i]);
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<int> res;
+        
+       int top = 0 , bottom = m -1 , left = 0 , right = n -1;
+       
+        while(top <= bottom && left <= right){
+            for(int i = left; i <= right; ++i) {
+                res.push_back(mat[top][i]);
             }
-            bottom--;
-        }
-
-        if (left <= right) {
-            // Traverse from bottom to top on the left column
-            for (int i = bottom; i >= top; --i) {
-                result.push_back(matrix[i][left]);
+            top++;
+            for(int i = top; i <= bottom; ++i) {
+                res.push_back(mat[i][right]);
             }
-            left++;
+            right--;
+            
+            if(top <= bottom) {
+                for(int i = right; i >= left; --i) {
+                    res.push_back(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            if(left <= right) {
+                for(int i = bottom; i >= top; --i) {
+                    res.push_back(mat[i][left]);
+                }
+                left++;
+            }
         }
-    }
-
-    return result;
+        return res;
+        
     }
 };
 
@@ -57,9 +52,10 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        vector<vector<int>> matrix(r);
 
         for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
@@ -70,6 +66,9 @@ int main() {
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
